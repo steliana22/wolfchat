f4dfd49ff812ee4b2b663e09345811610854adce
Cristian Tothazan <cristian.tothazan@globant.com>
2019-08-02 19:40:33 +0300

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">      
+        <form @submit.prevent="login" class="text-center">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Your name" name="name" v-model="name">
+                <p v-if="errorText" class="text-danger">{{ errorText }}</p>
+            </div>
+            <button type="submit" class="btn btn-primary">Start chatting</button>
+        </form>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data () {
+        return {
+            name: "",
+            errorText: null
+        }
+    },
+    methods: {
+        login() {
+            if (this.name) {
+                localStorage.setItem('username', this.name);
+                this.$router.push({name: 'Chat'});
+            } else {
+                this.errorText = "Please enter a name first!"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.login{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+form {
+    margin-top: 30%;
+}
+</style>
